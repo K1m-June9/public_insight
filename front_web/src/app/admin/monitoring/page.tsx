e39111a115c7aa0a90e256a5f0cef105443f1567
pumpkinbee <pumpkinbee2001@gmail.com>
2025-09-05 16:44:15 +0000

    키바나 대시보드

---
+++ front_web/src/app/admin/monitoring/page.tsx
@@ -0,0 +1,9 @@
+import KibanaDashboard from "@/components/admin/KibanaDashboard";
+
+export default function AdminMonitoringPage() {
+  return (
+    <div className="space-y-6">
+      <KibanaDashboard />
+    </div>
+  );
+}

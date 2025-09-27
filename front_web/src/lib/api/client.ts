a96a7e3b3152ad5c0c17105a569858eee9ed04a4
pumpkinbee <pumpkinbee2001@gmail.com>
2025-09-27 11:18:06 +0000

    개발환경이었던거다시수정

--- front_web/src/lib/api/client.ts
+++ front_web/src/lib/api/client.ts
@@ -27,9 +27,9 @@ export const apiClient = axios.create({
   // .env.local 파일에 NEXT_PUBLIC_API_URL을 설정
   // 예시: NEXT_PUBLIC_API_URL=http://localhost:8000/api/v1
     
-    //baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     //baseURL: '/api/v1',
-    baseURL:'http://localhost:8002/api/v1', // ha_frontend 개발전용(으악)
+    //baseURL:'http://localhost:8002/api/v1', // ha_frontend 개발전용(으악)
   // 다른 서버로 쿠키를 보내기 위한 설정
     withCredentials: true,
 });
